7436aa05a48608d1d36cd5aa2f7e25e8e390eef9
Chimoha31 <mokochii1108@gmail.com>
2022-06-02 15:35:15 -0700

    route, meta & public

---
+++ pages/posts/firstPost.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+import Link from "next/link";
+
+export default function firstPost() {
+  return (
+    <div>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <Link href="/">Go to Home</Link>
+    </div>
+  );
+}
